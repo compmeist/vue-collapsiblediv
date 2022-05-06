bd11ce16f1293aa85abdd8a791e0cfb1bf94e684
Nathan <radmaps@yahoo.com>
2022-05-06 15:25:52 -0500

    Create vue-collapsiblediv.js

---
+++ vue-collapsiblediv.js
@@ -0,0 +1,41 @@
+/*
+Usage: for VueJS v2
+ <collapsiblediv :showdiv.sync="myShowItBoolean">
+     <p>Stuff I would put in a div</p>
+ </collapsiblediv>
+*/
+
+   Vue.component('collapsiblediv',{ 
+      props: { 
+        showdiv:{ type:Boolean,default:true } // use :showdiv.sync=
+          ,showdivclass: {type:String, default:''}
+          ,showdivstyleobj: 
+           {type:Object, default:function() { return{}; }}
+         }
+      ,data: function() {
+        return { showdivd: this.showdiv };
+      }
+      ,computed: {
+       calcDubshowdivWrapperStyle: function() {
+          return { textAlign:'inherit' };
+        }
+      }
+      ,methods: {
+         togOpen: function() {
+           this.showdivd = !this.showdivd;
+           this.$emit('update:showdiv',this.showdivd);
+           this.$emit('open');
+        }
+        ,togClose: function() {
+           this.showdivd = !this.showdivd;
+           this.$emit('update:showdiv',this.showdivd);
+           this.$emit('close');
+        }
+      }
+      /* Unicode symbols: double left (xab) and double right arrow (xbb) */
+      ,template:`<span :style="calcDubshowdivWrapperStyle" >
+         &nbsp;<span v-if="!showdivd" @click="togOpen()">&#xbb;</span><span v-else="" @click="togClose()">&#xab;</span>
+         &nbsp;
+         <div v-show="showdivd" :class="showdivclass" :style="showdivstyleobj"><slot></slot></div>
+        </span>`
+    });
